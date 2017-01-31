c2c43bb9d9f267fa2b15c028d88ad496bcdb7774
Paul Steele <paul_steele@live.com>
2017-01-31 15:26:59 -0500

    some semblance of okayness

--- imports/functions/times.js
+++ imports/functions/times.js
@@ -1,6 +1,3 @@
-Handlebars.registerHelper('for', function(n, block) {
-    var accum = '';
-    for(var i = 0; i < n; ++i)
-        accum += 3;
-    return accum;
+Handlebars.registerHelper('for', function(n) {
+    return _.range(n);
 });
