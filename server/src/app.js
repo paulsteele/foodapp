3a4ecc82e52c627113ce724b2e96a6ffebe70bf8
Paul Steele <paul_steele@live.com>
2018-02-28 20:32:26 -0500

    add graphql server

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,24 @@
-const helloFunction = () => {
-  console.log('server hello');
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => ('Hello World'),
 };
 
-helloFunction();
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000);
+console.log('Running GraphQL API Server');
